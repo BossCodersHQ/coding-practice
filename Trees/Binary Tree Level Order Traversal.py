40dd3f6a2e94e4d976f34b77c847fba7df23e96a
Bosire Nyakundi <nyakundi@hotmail.co.uk>
2024-01-29 08:11:10 +0000

    Adding some tree pracitce

---
+++ Trees/Binary Tree Level Order Traversal.py
@@ -0,0 +1,35 @@
+from tree_node import TreeNode
+from typing import List
+
+import collections
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        q = collections.deque()
+        if root:
+            q.append(root)
+
+        while q:
+            val = []
+
+            for i in range(len(q)):
+                node = q.popleft()
+                val.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(val)
+        return res
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+
+    print(Solution().levelOrder(root))
