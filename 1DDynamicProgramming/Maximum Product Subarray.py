e7d4282640a186e3a9dafee29750edc3cfb2b62b
Bosire Nyakundi <nyakundi@hotmail.co.uk>
2024-06-20 10:40:04 +0100

    Adding maximum product redo

--- 1DDynamicProgramming/Maximum Product Subarray.py
+++ 1DDynamicProgramming/Maximum Product Subarray.py
@@ -3,20 +3,15 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_product = float("-inf")
-        curr_max_product = 1
-        curr_min_product = 1
+        ret = -float("inf")
+        curr_max = 1
+        curr_min = 1
         for num in nums:
-            if num == 0:
-                curr_max_product = 1
-                curr_min_product = 1
-                max_product = max(num, max_product)
-            else:
-                curr_max_product, curr_min_product = max(
-                    num, curr_max_product * num, curr_min_product * num
-                ), min(num, curr_max_product * num, curr_min_product * num)
-                max_product = max(curr_max_product, max_product)
-        return max_product
+            curr_max, curr_min = max(curr_max * num, num, curr_min * num), min(
+                curr_min * num, num, curr_max * num
+            )
+            ret = max(curr_max, ret)
+        return ret
 
 
 if __name__ == "__main__":
