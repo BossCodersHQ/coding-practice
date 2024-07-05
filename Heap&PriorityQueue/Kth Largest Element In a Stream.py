631f84caad0e701f5da2182cf2d598e2078957d3
Bosire Nyakundi <nyakundi@hotmail.co.uk>
2024-07-05 08:02:54 +0100

    Adding some heap training 💪

---
+++ Heap&PriorityQueue/Kth Largest Element In a Stream.py
@@ -0,0 +1,29 @@
+import bisect
+from typing import List
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = sorted(nums)
+        self.k = k
+
+    def add(self, val: int) -> int:
+        bisect.insort_left(self.nums, val)
+        return self.nums[-self.k]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+if __name__ == "__main__":
+    k = 3
+    arr = [4, 5, 8, 2]
+    kthLargest = KthLargest(k, arr)
+    assert kthLargest.add(3) == 4
+    assert kthLargest.add(5) == 5
+    assert kthLargest.add(10) == 5
+    assert kthLargest.add(9) == 8
+    assert kthLargest.add(4) == 8
+    print("Passed all test cases!")
