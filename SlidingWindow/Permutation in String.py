a8847401561a1845d48cdf95c7764371f726d413
Bosire Nyakundi <Nyakundi@hotmail.co.uk>
2024-07-07 16:36:30 +0100

    Using O(n) method instead of an O(26n) method

--- SlidingWindow/Permutation in String.py
+++ SlidingWindow/Permutation in String.py
@@ -1,27 +1,43 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        from collections import Counter
-
-        window = len(s1)  # 2
-        counts = Counter(s1)  # {a:1, b:1}
         if len(s1) > len(s2):
             return False
         if s1 == "":
             return True
-        curr_counts = Counter(s2[0:window])  # s2[0,2] = "ei" // {e:1,b:1}
-        if curr_counts == counts:
-            return True
-        for i in range(window, len(s2)):  # 2
-            char = s2[i]  # d
-            old_char = s2[i - window]  # s2[0] = e
-            curr_counts[char] = curr_counts.get(char, 0) + 1  # {e:1,b:1,d:1}
-            curr_counts[old_char] -= 1
-            if curr_counts.get(old_char) == 0:
-                del curr_counts[old_char]
 
-            if curr_counts == counts:
+        from collections import Counter
+
+        window = len(s1)
+        s1map = Counter(s1)
+        s2map = Counter(s2[0:window])
+
+        matches = 0
+        for i in range(0, 26):
+            c = chr(ord("a") + i)
+            if s1map.get(c, 0) == s2map.get(c, 0):
+                matches += 1
+
+        for i in range(window, len(s2)):  # 2
+            if matches == 26:
                 return True
-        return False
+            char = s2[i]  # d
+
+            s2map[char] = s2map.get(char, 0) + 1  # {e:1,b:1,d:1}
+
+            if s1map.get(char, 0) == s2map[char]:
+                matches += 1
+            elif s1map.get(char, 0) == s2map[char] - 1:
+                matches -= 1
+
+            old_char = s2[i - window]  # s2[0] = e
+            s2map[old_char] -= 1
+
+            if s1map.get(old_char, 0) == s2map[old_char]:
+                matches += 1
+            elif s1map.get(old_char, 0) == s2map[old_char] + 1:
+                matches -= 1
+
+        return matches == 26
 
 
 if __name__ == "__main__":
